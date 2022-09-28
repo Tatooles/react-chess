2b7527a7e5652c2fc9e3e22fa9c35ea71f3cc385
Kevin Tatooles <tatooles@wisc.edu>
2022-09-27 23:08:10 -0500

    Create vaguely working board

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,10 +1,25 @@
+import { Chess } from 'chess.js'
+import { useState } from 'react'
+
 const Board = () => {
-    return (
-        <div className="container">
-            <h2 className="text-8xl">test</h2>
-            Here is the board
-        </div>
-    )
+  const [chess, setChess] = useState(new Chess());
+
+  const makeMove = () => {
+    const newBoard = new Chess(chess.fen());
+    newBoard.move('e4');
+    setChess(newBoard);
+  }
+
+  return (
+    <div className="container">
+      <h2 className="text-8xl">test number 1</h2>
+      Here is the board
+      <button className='border-2' onClick={makeMove}>init chess</button>
+      {chess.board().map((piece, i) => (
+        <div key={i}>{JSON.stringify(piece)}</div>
+      ))}
+    </div>
+  )
 }
 
 export default Board
