49f98d96faba2a1c845a47193e3ba8aa0fe796df
Kevin Tatooles <tatooles@wisc.edu>
2022-10-06 23:23:17 -0500

    Add logic to play chess

--- src/Board.tsx
+++ src/Board.tsx
@@ -3,26 +3,46 @@ import { useState } from 'react'
 import Square from './Square';
 
 const Board = () => {
-  const [chess, setChess] = useState(new Chess());
+  const [board, setBoard] = useState(new Chess());
+  const [clickedPiece, setClickedPiece] = useState({ i: -1, square: '' });
 
-  const makeMove = () => {
-    const newBoard = new Chess(chess.fen());
-    newBoard.move('e4');
-    setChess(newBoard);
+  const makeMove = (from: string, to: string) => {
+    console.log(`Making move ${from} to ${to}`);
+    const newBoard = new Chess(board.fen());
+    const move = newBoard.move({ from: from, to: to });
+    if (!move) {
+      console.log('invalid move');
+    }
+    // TODO: Now need some validation to check for finished game or stalemate
+    setBoard(newBoard);
+  }
+
+  const indexToSquare = (i: number): string => {
+    return `${String.fromCharCode(i % 8 + 97)}${8 - Math.floor(i / 8)}`;
+  }
+
+  const squareClicked = (i: number, piece?: any) => {
+    // This means we have already selected a piece, so try to make a move
+    if (clickedPiece.i != -1) {
+      makeMove(clickedPiece.square, indexToSquare(i));
+      // Now deslect piece
+      setClickedPiece({ i: -1, square: '' });
+    }
+    // TODO: Don't want to select a pice on take
+    if (piece) {
+      setClickedPiece({ i: i, square: piece.square });
+      // TODO: Also select possible moves for this piece
+    }
   }
 
   return (
     <div>
-      <h2 className="text-8xl">test number 1</h2>
-      Here is the board
-      <button className='border-2' onClick={makeMove}>init chess</button>
-      {/* fixed size for different screens here */}
       <div id="board" className="grid grid-cols-8 bg-black w-[352px] h-[352px] md:w-[504px] md:h-[504px] mx-auto">
-        {chess.board().flat().map((piece, i) => (
+        {board.board().flat().map((piece, i) => (
           // Piece onClick will call a function with further logic to determine if it's the firist piece clicked
           // Maybe store the clicked piece in state
           // Later on that first click will highlight the possible moves for that piece
-          <Square key={i} i={i} piece={piece}></Square>
+          <Square squareClicked={squareClicked} active={i == clickedPiece.i ? true : false} key={i} i={i} piece={piece}></Square>
         ))}
       </div>
     </div>
--- src/Square.tsx
+++ src/Square.tsx
@@ -1,19 +1,22 @@
 import Piece from "./Piece"
 
 interface SquareProps {
+  squareClicked: Function;
+  active: boolean;
   i: number;
   piece: any;
 }
 
-const Square = ({ i, piece }: SquareProps) => {
+const Square = ({ squareClicked, active, i, piece }: SquareProps) => {
 
   const isBlack = (i: number) => {
     const x = i % 8;
     const y = Math.floor(i / 8);
     return (x + y) % 2 === 0;
   }
+
   return (
-    <div className={`w-[44px] h-[44px] md:w-[63px] md:h-[63px] text-center align-middle justify-items-center ${isBlack(i) ? "bg-[#ebebd0]" : "bg-[#779556]"}`}>
+    <div onClick={() => squareClicked(i, piece)} className={`w-[44px] h-[44px] md:w-[63px] md:h-[63px] text-center align-middle justify-items-center ${active ? 'border-2 border-red-600' : ''} ${isBlack(i) ? "bg-[#ebebd0]" : "bg-[#779556]"}`}>
       {piece
         ? <Piece piece={piece} />
         : <span>&nbsp;</span>
